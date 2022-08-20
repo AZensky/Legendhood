509b6d61fccf201178dff5a39178309c3493af4b
fangruz114 <101694480+fangruz114@users.noreply.github.com>
2022-08-20 12:25:13 -0700

    remove portfolio table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,5 @@
 from .db import db
 from .user import User
 from .transaction import Transaction
-from .portfolio import Portfolio
 from .watchlist import Watchlist
 from .watchlist_stock import WatchlistStock
