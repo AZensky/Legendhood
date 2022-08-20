7527c1ddcf17fb4f8012e5152436dd0bb456a13f
fangruz114 <101694480+fangruz114@users.noreply.github.com>
2022-08-20 02:11:32 -0700

    login, demouser, logout

--- react-app/src/components/DashboardNavbar/index.js
+++ react-app/src/components/DashboardNavbar/index.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as RobinhoodLogo } from "../../assets/RBLogo.svg";
-import "./DashboardNav.css";
+import { useDispatch } from 'react-redux';
+import { logout } from '../../store/session';
+import { useHistory } from 'react-router-dom';
+import './DashboardNav.css'
 
 function DashboardNav() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const onLogout = async (e) => {
+    await dispatch(logout())
+      .then(history.push('/'));
+  };
+
   return (
     <div className="dashboard-nav-container">
       {/* left section of navbar */}
@@ -10,19 +22,22 @@ function DashboardNav() {
         <div className="rb-logo-container">
           <RobinhoodLogo className="dashboard-rb-logo" />
         </div>
-        <form className="stock-search-form">
-          <i className="fa-solid fa-magnifying-glass"></i>
-          <label>
-            <input type="text" className="stock-search" placeholder="Search" />
-          </label>
-        </form>
+        <div className="stock-search-form">
+          <i class="fa-solid fa-magnifying-glass"></i>
+          <input
+            type="text"
+            className="stock-search"
+            placeholder="Search"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
       </div>
 
       {/* right side of navbar */}
       <div className="social-links-account-container">
-        <p>Github</p>
-        <p>Account</p>
-        <p>Logout</p>
+        <a href='https://github.com/AZensky/RobinhoodClone'>Github</a>
+        <button>Account</button>
+        <button onClick={onLogout}>Logout</button>
       </div>
     </div>
   );
