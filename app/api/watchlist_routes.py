d166f7a68c69c3db40828c32b3a09ccfa9f40ad3
Yue Huang <wongyue22@gmail.com>
2022-08-21 22:46:16 -0400

    get all watchlists fetch and updates css

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -6,8 +6,15 @@ from app.models import Watchlist
 watchlist_routes = Blueprint('watchlist', __name__)
 
 
+@watchlist_routes.route('')
+# @login_required
+def get_watchlists():
+    watchlists = Watchlist.query.all()
+    return {'watchlists': [watchlist.to_dict() for watchlist in watchlists]}
+
+
 @watchlist_routes.route('/<int:id>')
 # @login_required
-def get_watchlist(id):
+def get_watchlist_by_id(id):
     watchlist = Watchlist.query.get(id)
     return watchlist.to_dict()
