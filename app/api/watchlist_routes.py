ef1f7075b7e79d0d86bfe8d21af96a004aadfe62
alex <alexzelinsky124@gmail.com>
2022-08-22 15:18:31 -0700

    worked on watchlist routes

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -23,10 +23,13 @@ def get_watchlist_by_id(id):
     return watchlist.to_dict() if watchlist else {"id": id, "watchlistStocks": []}
 
 
-@watchlist_routes.route('/<int:id>/<symbol>/delete')
+@watchlist_routes.route('/<int:id>/stocks/<symbol>', methods=['DELETE', 'GET'])
 @login_required
-def delete_watchlist_by_id(id):
+def delete_watchlist_stock_by_id(id, symbol):
     watchlist = Watchlist.query.filter(
         Watchlist.user_id == current_user.id,  Watchlist.id == id).one_or_none()
+
+    print(watchlist)
+
     # else should throw 404
-    return watchlist.to_dict() if watchlist else {"id": id, "watchlistStocks": []}
+    return watchlist.to_dict() if watchlist else {"id": id, "watchlistStocks": []}, 404
