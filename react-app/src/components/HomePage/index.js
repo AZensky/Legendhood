d1c499861efd9bfd4f7f078a3dab2284361dc7ea
alex <alexzelinsky124@gmail.com>
2022-08-22 14:09:15 -0700

    made redirects when user is not logged in

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,19 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import HomePageNav from "./HomePageNav";
 import HomePageTopSec from "./HomePageTopSec";
-import HomePageInvestSec from './HomePageInvestSec'
-import './HomePage.css'
+import HomePageInvestSec from "./HomePageInvestSec";
+import "./HomePage.css";
 import HomePageCryptoSec from "./HomePageCryptoSec";
 import HomePageGnteeSec from "./HomePageGnteeSec";
 import HomePageJoinSec from "./HomePageJoinSec";
 import HomePageFooter from "./HomePageFooter";
 
 function HomePage() {
+  const history = useHistory();
+  const user = useSelector((state) => state.session.user);
+
+  if (user) history.push("/dashboard");
+
   return (
     <>
       <HomePageNav />
       <HomePageTopSec />
-      <div className='free-stock'>
+      <div className="free-stock">
         <p>Get your first stock free. Limitations apply.</p>
       </div>
       <HomePageInvestSec />
