115269992f3b6360a4809dc7f4b96e0ab619e8de
Yue Huang <wongyue22@gmail.com>
2022-08-23 17:06:58 -0400

    create and edit watchlist completed

--- react-app/src/components/WatchListPage/index.js
+++ react-app/src/components/WatchListPage/index.js
@@ -5,6 +5,8 @@ import {
     clearCurrentWatchlist,
     getWatchlist,
     loadWatchlists,
+    createOneWatchlist,
+    editOneWatchlist,
     deleteOneStock
 } from "../../store/watchlist";
 import DashboardNav from "../DashboardNavbar";
@@ -18,6 +20,8 @@ function WatchListPage() {
     const dispatch = useDispatch();
     const history = useHistory();
     const [isShown, setIsShown] = useState(false);
+    const [isEditShown, setIsEditShown] = useState(true);
+    const [name, setName] = useState("");
 
     //get watchlist by id
     const { watchlistId } = useParams();
@@ -62,11 +66,29 @@ function WatchListPage() {
         history.push(`/watchlists/${listId}`);
     }
 
-    function createList() {
+    function showCreateList() {
         // dispatch
         setIsShown(current => !current);
 
     }
+
+    function clickCancel() {
+        setIsShown(false)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await dispatch(createOneWatchlist({ name }))
+            .then((watchlist) => history.push(`/watchlists/${watchlist?.id}`))
+        setIsShown(false)
+    };
+
+    const handleEditSubmit = async (e) => {
+        e.preventDefault();
+        await dispatch(editOneWatchlist(watchlistId, { name }))
+            .then((watchlist) => history.push(`/watchlists/${watchlist?.id}`))
+        setIsEditShown(false)
+    };
 
     function deleteStock(e) {
         const stocksym = e.currentTarget.id;
@@ -88,15 +110,30 @@ function WatchListPage() {
                         </div>
 
                         <div className="watchlist-scroll-title">
-                            <div className="watchlist-scroll-listname">{watchlist.name}</div>
+                            {!isEditShown && <div className="watchlist-scroll-listname">{watchlist?.name}</div>}
+                            {isEditShown && <div>
+                                <form onSubmit={handleEditSubmit}>
+                                    <input
+                                        type="text"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        required>
+                                    </input>
+                                    <button type="submit" >
+                                        yes
+                                    </button>
+                                </form>
+
+
+                            </div>}
                             <div className="watchlist-scroll-ellipsisicon">
-                                <WatchListDropdown watchlistName={watchlist.name} watchlist_Id={watchlistId} />
+                                <WatchListDropdown watchlistName={watchlist?.name} watchlist_Id={watchlistId} />
                             </div>
                         </div>
 
                         <div className="watchlist-itemnum">
                             {" "}
-                            {watchlist.watchlistStocks.length} items
+                            {watchlist?.watchlistStocks.length} items
                         </div>
                         <div className="watchlist-table">
                             <header className="watchlist-row">
@@ -107,7 +144,7 @@ function WatchListPage() {
                                 <div>Market Cap</div>
                                 <div></div>
                             </header>
-                            {watchlist.watchlistStocks.map((stock) => (
+                            {watchlist?.watchlistStocks.map((stock) => (
                                 <div id={stock.symbol} className="watchlist-row">
                                     <div onClick={ClickStock}>{stock.name}</div>
                                     <div onClick={ClickStock}>{stock.symbol}</div>
@@ -132,7 +169,7 @@ function WatchListPage() {
                         <div className="watchlist-sticky-title">
                             <div className="watchlist-sticky-list">Lists</div>
                             <div className="watchlis-sticky-plus-sign">
-                                <button className="watchlist-button" onClick={createList}>+</button>
+                                <button className="watchlist-button" onClick={showCreateList}>+</button>
 
                             </div>
                         </div>
@@ -141,27 +178,36 @@ function WatchListPage() {
                             <div className="watchlist-createlist-dropdown">
 
                                 <div className="watchlist-createlist-dropdown-row">
-                                    <div>
-                                        <img
-                                            className="watchlist-lightning-logo-2"
-                                            alt="⚡"
-                                            src="https://cdn.robinhood.com/emoji/v0/128/26a1.png"
-                                        ></img>
-                                    </div>
-                                    <div>
-                                        <form>
-                                            <input>
-                                            </input>
-                                        </form>
-                                    </div>
-                                </div>
 
-                                <div className="watchlist-createlist-dropdown-row">
                                     <div>
-                                        cancel
-                                    </div>
-                                    <div>
-                                        create list
+                                        <div>
+                                            <img
+                                                className="watchlist-lightning-logo-2"
+                                                alt="⚡"
+                                                src="https://cdn.robinhood.com/emoji/v0/128/26a1.png"
+                                            ></img>
+                                        </div>
+                                        <div>
+                                            <form onSubmit={handleSubmit}>
+                                                <input
+                                                    type="text"
+                                                    value={name}
+                                                    onChange={(e) => setName(e.target.value)}
+                                                    required
+                                                >
+                                                </input>
+                                                <div className="watchlist-createlist-dropdown-row">
+                                                    <div>
+                                                        <button type="button" className="watchlist-button"
+                                                            onClick={clickCancel} >Cancel</button>
+                                                    </div>
+                                                    <div>
+                                                        <button type="submit" className="watchlist-button" >Create List</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+
                                     </div>
                                 </div>
                             </div>
