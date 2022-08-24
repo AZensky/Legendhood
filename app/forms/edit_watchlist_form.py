4d60b8a6d334541403a89d2660c1b6ab92bf05b9
Yue Huang <wongyue22@gmail.com>
2022-08-24 10:56:42 -0400

    watchlist backend validations done

--- app/forms/edit_watchlist_form.py
+++ app/forms/edit_watchlist_form.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+def watchlist_name_length(form, field):
+    watchlist_name = field.data
+    if len(watchlist_name) >15:
+        raise ValidationError("Watchlist's name must be 15 characters or less.")
 
 class EditWatchlistForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired(), watchlist_name_length])
